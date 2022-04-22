932e0d3d8cf4af2200f9d15ccec8799bedd87acd
giou55 <giourmetakis00@gmail.com>
2022-04-22 22:51:52 +0300

    new commit

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using API.Interfaces;
+using API.Entities;
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user){
+            return "hjhj";
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,6 +32,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             // the order of services here doesn't matter
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options => 
             {
                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
